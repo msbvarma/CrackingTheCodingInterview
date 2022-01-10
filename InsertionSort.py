de499f39d5ffe15bb2513ae4573ce00c51e06538
msbvarma <msbvarma@users.noreply.github.com>
2022-01-10 06:54:20 +0530

    Sorting-InsertionSort

---
+++ InsertionSort.py
@@ -0,0 +1,8 @@
+def insertionSort(arr):
+    for j in range(1,len(arr)):
+        key = arr[j]
+        i = j-1
+        while i>=0 and arr[i] > key:
+            arr[i+1] = key
+            i = i-1
+        arr[i+1] = key
